a1d19a9b9c71b0e70e831b892b019a86ca87532f
vlee4 <vicglee1234007@gmail>
2021-07-22 17:34:54 -0500

    Create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+require("webpack");
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ], 
+  },  
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html"
+    }),
+  ]
+}
