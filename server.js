1209ac43d0e3fd4f46616dcc8f3298758412afcd
vlee4 <vicglee1234007@gmail>
2021-07-23 17:50:46 -0500

    Experiment with explicit importing of webpack setup upon starting server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+// const webpack = require('webpack')
+// const config = require('./webpack.config.js')
+// const compiler = webpack(config);
+
 
 const path = require('path');
 const PORT = process.env.PORT || 3000;
@@ -10,6 +16,20 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html');
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static('dist'));
+
+// app.use(
+//     require('webpack-dev-middleware')(compiler, {
+//       publicPath: config.output.publicPath,
+//     })
+//   );
+  
+//   app.use(
+//     require(`webpack-hot-middleware`)(compiler, {
+//       log: false,
+//       path: `/__webpack_hmr`,
+//       heartbeat: 10 * 1000,
+//     })
+//   );
 
 app.get('/', (req, res)=>{
     res.sendFile(HTML_FILE, (err)=>{
