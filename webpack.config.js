b3ea2a478df7bfdba138825881447ba41f059014
vlee4 <vicglee1234007@gmail>
2021-07-23 00:21:54 -0500

    Modify app entry point & add scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./client/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve("dist"),
